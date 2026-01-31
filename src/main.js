806d75582751c1d11d916ff2a8b46ef65dcabee1
Florian <florian.caplet@Florians-MacBook-Air.local>
2026-01-31 14:01:26 +0100

    Feat : back dash

--- src/main.js
+++ src/main.js
@@ -1,43 +1,64 @@
-// import { createboss } from './boss.js';
 import { movePlayer } from './player1.js';
 import { createPlayer } from './player1.js';
+import { createBoss } from './ennemis/boss.js';
+import { jumpBoss } from './ennemis/boss.js';
+import { moveBoss } from './ennemis/boss.js';
+
 
 const config = {
-  type: Phaser.AUTO,
-  width: window.innerWidth,
-  height: window.innerHeight,
-  backgroundColor: '#000000',
-  physics: {
-    default: 'arcade',
-    arcade: {
-        gravity: { y: 600 },
-        debug: false
+    type: Phaser.AUTO,
+    width: window.innerWidth,
+    height: window.innerHeight,
+    backgroundColor: '#000000',
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { y: 600 },
+            debug: false
+        }
+    },
+    scale: {
+        mode: Phaser.Scale.RESIZE,
+        autoCenter: Phaser.Scale.CENTER_BOTH,
+    },
+    scene: {
+        preload,
+        create,
+        update
     }
-  },
-  scale: {
-    mode: Phaser.Scale.RESIZE,
-    autoCenter: Phaser.Scale.CENTER_BOTH,
-  },
-  scene: {
-    preload,
-    create,
-    update
-  }
 };
 
 new Phaser.Game(config);
 
 function preload() {
-  // ici mon mets nos assets img, sons, maps etc...
+    this.load.tilemapTiledJSON('maMap', '/assets/map.json');
+    this.load.image('mesTuiles', '/assets/fond.jpg');
 }
 
 function create() {
-    createPlayer.call(this);
-  //  createboss.call(this);
-  // la nos objets genre joueur, collisions etc...
-}
+    const map = this.make.tilemap({ key: 'maMap' });
 
+    const tileset = map.addTilesetImage('fond', 'mesTuiles');
+
+    const sol = map.createLayer('Calque de Tuiles 1', tileset, 0, 0);
+
+    if (sol) {
+        sol.setCollisionByProperty({ collides: true });
+    }
+
+    createPlayer.call(this);
+
+    if (this.player && sol) {
+        this.physics.add.collider(this.player, sol);
+    }
+
+    let isDashing = true;
+
+    createBoss.call(this);
+    this.jumpBoss = jumpBoss.bind(this);
+}
 function update() {
     movePlayer.call(this);
-  // ici tout ce qui va se jouer a chaque frame
+    moveBoss.call(this);
+    // ici tout ce qui va se jouer a chaque frame
 }
--- src/player1.js
+++ src/player1.js
@@ -1,16 +1,20 @@
 export function createPlayer() {
-        this.player = this.add.rectangle(100, 100, 50, 50, 0x00ff00);
-        this.player.x = window.innerWidth / 2;
-        this.physics.add.existing(this.player);
-        this.player.body.setCollideWorldBounds(true);
-        this.cursors = this.input.keyboard.createCursorKeys();
+    this.player = this.add.rectangle(100, 100, 50, 50, 0x00ff00);
+    this.player.x = window.innerWidth / 2;
+    this.physics.add.existing(this.player);
+    this.player.body.setCollideWorldBounds(true);
+    this.cursors = this.input.keyboard.createCursorKeys();
+    this.player.isDashing = false;
+    this.keyQ = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.Q);
 }
-
 
 export function movePlayer() {
     if (!this.player || !this.player.body) return;
 
-    // la marche
+    if (this.player.isDashing) {
+        return;
+    }
+
     if (this.cursors.left.isDown) {
         this.player.body.setVelocityX(-300);
     } else if (this.cursors.right.isDown) {
@@ -19,26 +23,35 @@ export function movePlayer() {
         this.player.body.setVelocityX(0);
     }
 
-    // gerer le sol
-    // Si on touche le sol, on remet le compteur à 0
     if (this.player.body.onFloor()) {
         this.jumpCount = 0;
     }
 
-    // double saut
-    // JustDown vérifie si on vient de cliquer sur "Haut" à cet instant précis
     const isJumpJustPressed = Phaser.Input.Keyboard.JustDown(this.cursors.up);
 
     if (isJumpJustPressed) {
-        // 1 saut : on est au sol
         if (this.player.body.onFloor()) {
             this.player.body.setVelocityY(-450);
             this.jumpCount = 1;
         }
-        // 2 saut : on est en l'air et on n'a sauté qu'une fois
         else if (this.jumpCount === 1) {
             this.player.body.setVelocityY(-450);
             this.jumpCount = 2;
         }
+    }
+
+    if (Phaser.Input.Keyboard.JustDown(this.keyQ)) {
+        this.player.isDashing = true;
+
+        this.player.body.setVelocityX(-1000);
+
+        this.time.addEvent({
+            delay: 200,
+            callback: () => {
+                this.player.isDashing = false;
+                this.player.body.setVelocityX(0);
+            },
+            callbackScope: this
+        });
     }
 }
