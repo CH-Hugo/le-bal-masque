566a6acf5a5eee05dc47665ea6538e310177d294
CH-Hugo <hugo.chabod@gmail.com>
2026-01-31 10:58:40 +0100

    feat: player1 - fix velocity initialization and import statements

--- src/player1.js
+++ src/player1.js
@@ -1,6 +1,5 @@
 export function createPlayer() {
         this.player = this.add.rectangle(100, 100, 50, 50, 0x00ff00);
-        this.player.y = window.innerHeight / 2;
         this.player.x = window.innerWidth / 2;
         this.physics.add.existing(this.player);
         this.player.body.setCollideWorldBounds(true);
@@ -15,9 +14,11 @@ export function movePlayer() {
         this.player.body.setVelocityX(-300);
     } else if (this.cursors.right.isDown) {
         this.player.body.setVelocityX(300);
-    } else if (this.cursors.up.isDown) {
-        this.player.body.setVelocityY(-200);
     } else {
-        this.player.body.setVelocity(0);
+        this.player.body.setVelocityX(0); 
+    }
+
+    if (this.cursors.up.isDown && this.player.body.blocked.down) {
+        this.player.body.setVelocityY(-450);
     }
 }
